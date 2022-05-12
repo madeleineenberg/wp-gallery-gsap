24e920f71ee3936081bfdced9832ab7180b83b1b
madeleineenberg <madeleine_enberg@hotmail.com>
2022-05-12 09:12:11 +0200

    add: footer

--- web/app/themes/osolo/resources/views/layouts/app.blade.php
+++ web/app/themes/osolo/resources/views/layouts/app.blade.php
@@ -3,13 +3,7 @@
 <div class="main-container" data-scroll-container>
     <main class="main">
         @yield('content')
+
     </main>
-
-    @hasSection('sidebar')
-        <aside class="sidebar">
-            @yield('sidebar')
-        </aside>
-    @endif
 </div>
-
 @include('partials.footer')
--- web/app/themes/osolo/resources/views/partials/footer.blade.php
+++ web/app/themes/osolo/resources/views/partials/footer.blade.php
@@ -1,5 +1,15 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
-  </div>
+<footer class="footer">
+    <div class="footer__container">
+        <div class="footer__email">
+            <a class="footer-links" href="/">email</a>
+        </div>
+        <div class="footer__social">
+            <ul class="footer__social-list">
+                <li> <a class="footer-links" href="/">instagram</a></li>
+                <li> <a class="footer-links" href="/">github</a></li>
+                <li> <a class="footer-links" href="/">linkedin</a></li>
+            </ul>
+
+        </div>
+    </div>
 </footer>
