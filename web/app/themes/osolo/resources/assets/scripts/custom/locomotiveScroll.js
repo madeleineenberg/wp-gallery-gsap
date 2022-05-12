67a12143c4cd3120d99ab83ed01d1e0741143ba0
madeleineenberg <madeleine_enberg@hotmail.com>
2022-05-12 09:13:20 +0200

    fix: update locomotive scroll after image load

--- web/app/themes/osolo/resources/assets/scripts/custom/locomotiveScroll.js
+++ web/app/themes/osolo/resources/assets/scripts/custom/locomotiveScroll.js
@@ -1,10 +1,17 @@
 import LocomotiveScroll from 'locomotive-scroll';
+import imagesLoaded from 'imagesloaded';
+
 export default function () {
   if (!$('body').hasClass('home') && !$('body').hasClass('single-galleri')) {
+    let scrollContainer = document.querySelector('[data-scroll-container]');
+
     const scroll = new LocomotiveScroll({
-      el: document.querySelector('[data-scroll-container]'),
+      el: scrollContainer,
       smooth: true,
       smoothClass: 'has-scroll-smooth',
+    });
+    imagesLoaded(scrollContainer, { background: '.item' }, function () {
+      scroll.update();
     });
   }
 }
