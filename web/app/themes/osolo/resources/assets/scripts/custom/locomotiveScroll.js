eaa7c71736b66165be5a48a12ba48c169661a529
madeleineenberg <madeleine_enberg@hotmail.com>
2022-05-30 20:43:46 +0200

    add: function to update locomotive scroll

--- web/app/themes/osolo/resources/assets/scripts/custom/locomotiveScroll.js
+++ web/app/themes/osolo/resources/assets/scripts/custom/locomotiveScroll.js
@@ -10,8 +10,41 @@ export default function () {
       smooth: true,
       smoothClass: 'has-scroll-smooth',
     });
+
+    //update scroll when images are loaded
+
     imagesLoaded(scrollContainer, { background: '.item' }, function () {
       scroll.update();
+    });
+
+    //update locomotive scroll if height is changed
+
+    const containerId = document.querySelector('[data-scroll-container]');
+    let MutationObserver =
+      window.MutationObserver ||
+      window.webkitMutationObserver ||
+      window.MozMutationObserver;
+
+    let recordHeight = 0;
+    const mutationObserver = new MutationObserver(function (mutations) {
+      let height = window
+        .getComputedStyle(containerId)
+        .getPropertyValue('height');
+      if (height === recordHeight) {
+        return;
+      }
+      recordHeight = height;
+
+      console.log(' Height is Change');
+
+      scroll.update();
+    });
+
+    mutationObserver.observe(containerId, {
+      childList: true,
+      attributes: true,
+      characterData: true,
+      subtree: true,
     });
   }
 }
