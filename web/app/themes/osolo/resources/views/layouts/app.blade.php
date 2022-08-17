2e340998ec30e6772985478487559c04f255d718
madeleineenberg <madeleine_enberg@hotmail.com>
2022-08-17 19:21:28 +0200

    fix: styling

--- web/app/themes/osolo/resources/views/layouts/app.blade.php
+++ web/app/themes/osolo/resources/views/layouts/app.blade.php
@@ -1,7 +1,12 @@
 @include('partials.header')
 
+@php
+global $post;
+$post_slug = $post->post_name;
+@endphp
+
 <div class="main-container" data-scroll-container>
-    <main class="main">
+    <main class="main {{ $post_slug }}">
         @yield('content')
 
     </main>
