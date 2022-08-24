e969c2d0d9850fb17f05883d0a2c43b3f2ed4ee4
madeleineenberg <madeleine_enberg@hotmail.com>
2022-08-24 20:24:55 +0200

    fix text-block styling

--- web/app/themes/osolo/resources/views/blocks/text-block.blade.php
+++ web/app/themes/osolo/resources/views/blocks/text-block.blade.php
@@ -1,9 +1,9 @@
 {{-- /
   Title: Text block
-  Description: Hover link effect with image
+  Description: Text with animations
   Category: formatting
   Icon: admin-comments
-  Keywords: Hover, link, image
+  Keywords: text
   Mode: auto
   Align: center
   PostTypes: page post
@@ -15,7 +15,7 @@
 <div class="c-text-block">
     <h2 class="c-text-block__heading" id="headline" data-scroll data-scroll-repeat>{{ $heading }}</h2>
     <div class="underline" data-scroll></div>
-    <div class="c-text-block__text" data-scroll data-scroll-speed="5">
+    <div class="c-text-block__text" data-scroll data-scroll-speed="-2">
         <p>{{ $text }}
             @if ($icon)
                 <ion-icon name="{{ $icon }}"></ion-icon>
