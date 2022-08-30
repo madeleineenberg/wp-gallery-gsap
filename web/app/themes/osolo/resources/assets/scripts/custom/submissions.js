9d5b3ee3902dd0791c2e8ccc560d9a29e4b49c25
madeleineenberg <madeleine_enberg@hotmail.com>
2022-08-30 20:25:20 +0200

    fix: error in submission

--- web/app/themes/osolo/resources/assets/scripts/custom/submissions.js
+++ web/app/themes/osolo/resources/assets/scripts/custom/submissions.js
@@ -4,7 +4,6 @@ export default function () {
       url: '/wp-json/portfolio/submissions',
       success: (response) => {
         if (response) {
-          console.log(response);
           $('.c-note-board__grid').append(response.html);
           $('.c-note-board__grid').addClass('notes-ready');
 
@@ -27,19 +26,20 @@ export default function () {
     });
   }, 100);
 
-  $(document).on('mousemove', function (event) {
-    var eye = $('.eye');
-    console.log('hej');
-    console.log('eye', eye);
-    var x = eye.offset().left + eye.width() / 2;
-    var y = eye.offset().top + eye.height() / 2;
-    var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = rad * (180 / Math.PI) * -4 + 180;
-    eye.css({
-      '-webkit-transform': 'rotate(' + rot + 'deg)',
-      '-moz-transform': 'rotate(' + rot + 'deg)',
-      '-ms-transform': 'rotate(' + rot + 'deg)',
-      transform: 'rotate(' + rot + 'deg)',
+  //eyes follows mousemove
+  if ($('.body').hasClass('contact')) {
+    $(document).on('mousemove', function (event) {
+      let eye = $('.eye');
+      let x = eye.offset().left + eye.width() / 2;
+      let y = eye.offset().top + eye.height() / 2;
+      let rad = Math.atan2(event.pageX - x, event.pageY - y);
+      let rot = rad * (180 / Math.PI) * -4 + 180;
+      eye.css({
+        '-webkit-transform': 'rotate(' + rot + 'deg)',
+        '-moz-transform': 'rotate(' + rot + 'deg)',
+        '-ms-transform': 'rotate(' + rot + 'deg)',
+        transform: 'rotate(' + rot + 'deg)',
+      });
     });
-  });
+  }
 }
