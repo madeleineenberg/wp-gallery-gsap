3f670bd3f641a35a282373ba684bb5758e7fa464
madeleineenberg <madeleine_enberg@hotmail.com>
2022-10-06 09:25:20 +0200

    fix: remove cusom cursor

--- web/app/themes/osolo/resources/assets/scripts/custom/customCursor.js
+++ web/app/themes/osolo/resources/assets/scripts/custom/customCursor.js
@@ -1,15 +1,17 @@
 import Cursor from 'custom-cursor';
+// import { dpkCursor } from 'dpk_cursor/src/dpkCursor';
 
 export default function () {
-  new Cursor({
-    count: 2,
-    targets: [
-      'a',
-      '.navigation-title',
-      '.menu-open',
-      '.menu-close',
-      '.gallery-title',
-      '.split-child',
-    ],
-  });
+  // new Cursor({
+  //   count: 2,
+  //   targets: [
+  //     'a',
+  //     '.navigation-title',
+  //     '.menu-open',
+  //     '.menu-close',
+  //     '.gallery-title',
+  //     '.split-child',
+  //   ],
+  // });
+  // const customCursor = new dpkCursor({ ease: 0.25 });
 }
