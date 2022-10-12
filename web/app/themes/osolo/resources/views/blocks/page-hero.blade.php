bd330e08165502fa69483f5e9a93af45db93cee7
madeleineenberg <madeleine_enberg@hotmail.com>
2022-10-12 12:47:43 +0200

    fix: opacity on page hero image

--- web/app/themes/osolo/resources/views/blocks/page-hero.blade.php
+++ web/app/themes/osolo/resources/views/blocks/page-hero.blade.php
@@ -16,9 +16,9 @@
 
 <div class="c-page-hero">
     <div class="wrapper">
-        <div class="c-page-hero_image-wrapper">
-            <img src="{{ $image['sizes']['large'] }}" alt="" class="c-page-hero__image" data-scroll
+        <div class="c-page-hero_image-wrapper" data-scroll
                 data-scroll-speed="2">
+            <img src="{{ $image['sizes']['large'] }}" alt="" class="c-page-hero__image" data-scroll>
         </div>
         <h1 class="c-page-hero__title" data-scroll data-scroll-speed="-2">{{ $title }}</h1>
     </div>
