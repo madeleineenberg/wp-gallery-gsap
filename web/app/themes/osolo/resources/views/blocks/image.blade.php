e0cf87dcc3d3c3a2dee95db589f77da26c6c7428
madeleineenberg <madeleine_enberg@hotmail.com>
2022-11-28 20:19:39 +0100

    fix: image block width size

--- web/app/themes/osolo/resources/views/blocks/image.blade.php
+++ web/app/themes/osolo/resources/views/blocks/image.blade.php
@@ -15,12 +15,12 @@
 <div class="c-image-video-block {{ $placement[0] ? $placement[0] : '' }}">
     @if ($image)
         <div class="c-image-video-block__image-wrapper" data-scroll data-scroll-speed="-2">
-            <img src="{{ $image['sizes']['large'] }}" style="opacity: {{$opacity}}" />
+            <img src="{{ $image['sizes']['large'] }}" style="opacity: {{$opacity}}" data-scroll/>
         </div>
     @endif
     @if ($video)
         <div class="c-image-video-block__image-wrapper">
-            <video autoplay loop muted playsinline style="opacity: {{$opacity}}">
+            <video autoplay loop muted playsinline style="opacity: {{$opacity}} " data-scroll>
                 <source src="{{ $video['url'] }}" type="video/mp4">
                 <source src="{{ $video['url'] }}" type="video/webm">
             </video>
