68feeeb60488ec61e17830a0ac3970097cd8534e
madeleineenberg <madeleine_enberg@hotmail.com>
2022-11-28 15:19:26 +0100

    fix: opacity and video option on page hero block

--- web/app/themes/osolo/app/View/Composers/PageHero.php
+++ web/app/themes/osolo/app/View/Composers/PageHero.php
@@ -19,7 +19,9 @@ class PageHero extends Composer
     public function with()
     {
         return [
+            'opacity' => get_field('image_opacity') ? get_field('image_opacity') : '1',
            'image' => get_field('image'),
+           'video' => get_field('video_background'),
            'title' => get_field('title'),
         ];
     }
--- web/app/themes/osolo/resources/views/blocks/page-hero.blade.php
+++ web/app/themes/osolo/resources/views/blocks/page-hero.blade.php
@@ -18,7 +18,15 @@
     <div class="wrapper">
         <div class="c-page-hero_image-wrapper" data-scroll
                 data-scroll-speed="2">
-            <img src="{{ $image['sizes']['large'] }}" alt="" class="c-page-hero__image" data-scroll>
+            @if($image)
+                <img src="{{ $image['sizes']['large'] }}" alt="" class="c-page-hero__image" style="opacity: {{$opacity}}" data-scroll>
+            @endif
+            @if($video)
+                <video autoplay loop muted playsinline style="opacity: {{$opacity}}" data-scroll>
+                    <source src="{{ $video['url'] }}" type="video/mp4">
+                    <source src="{{ $video['url'] }}" type="video/webm">
+                </video>
+            @endif
         </div>
         <h1 class="c-page-hero__title" data-scroll data-scroll-speed="-2">{{ $title }}</h1>
     </div>
