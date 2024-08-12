f6dcbad436eb8bd73855de61950fc86c480f8d54
madeleineenberg <madeleine_enberg@hotmail.com>
2024-08-12 09:53:07 +0200

    chore: Update footer links

--- web/app/themes/osolo/resources/views/partials/footer.blade.php
+++ web/app/themes/osolo/resources/views/partials/footer.blade.php
@@ -2,34 +2,34 @@
     <div class="footer__container">
 
         <div class="footer__email">
-           
-            <a class="footer-links link" href="/"> 
+
+            <a class="footer-links link" href="mailto:info@madeleineenberg.com">
                 <div>
                     <span class="link-text">email</span>
                 </div>
             </a>
-          
+
         </div>
 
         <div class="footer__social">
-            
-                 <a class="footer-links link" href="/">
+
+                 <a class="footer-links link" href="https://www.instagram.com/madeleineenberg/" target="_blank" rel="nofollow">
                     <div>
                         <span class="link-text">instagram</span>
                     </div>
                 </a>
-                 <a class="footer-links link" href="/">
+                 <a class="footer-links link" href="https://github.com/madeleineenberg" target="_blank" rel="nofollow">
                     <div>
                         <span class="link-text">github</span>
                     </div>
                 </a>
-                 <a class="footer-links link" href="/">
+                 <a class="footer-links link" href="https://www.linkedin.com/in/madeleine-enberg/" target="_blank" rel="nofollow">
                     <div>
                         <span class="link-text">linkedin</span>
                     </div>
                 </a>
-               
-       
+
+
 
         </div>
     </div>
